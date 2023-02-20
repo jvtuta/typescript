1c71fa54d03b2f15ddd227fec520ef96f21fb683
jvtuta <joaovoliveira2399@gmail.com>
2023-02-20 11:54:45 -0300

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,10 @@ module.exports = {
     node: true,
   },
   extends: [
-    'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'plugin:prettier/recommended'
   ],
   globals: {
     Atomics: 'readonly',
--- index.ts
+++ index.ts
@@ -1,3 +1,1 @@
-const nome = 'Luiz';
-
- 
+const nome = 'João';
